bece9e9021f0a1395db304a1b57dc6481f3dffb8
foxchristian4 <foxchristian4@gmail.com>
2015-11-18 09:38:07 -0500

    i did stuff

--- app/src/main/java/com/example/ryangress/buildatrump/MainActivity.java
+++ app/src/main/java/com/example/ryangress/buildatrump/MainActivity.java
@@ -1,31 +1,42 @@
 package com.example.ryangress.buildatrump;
 
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toolbar;
 
 
 public class MainActivity extends ActionBarActivity {
 
-    private android.widget.Toolbar toolbar;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.splash);
+        Button b = (Button)findViewById(R.id.btnSplash);
+        b.setOnClickListener(new View.OnClickListener() {
 
-        toolbar = (Toolbar) findViewById(R.id.app_bar);
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MainActivity.class));
+            }
 
-        setSupportActionBar(toolbar);
-    }
+
+        });}
+
+    private android.widget.Toolbar toolbar;
 
     private void setSupportActionBar(Toolbar toolbar) {
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
 
+        setSupportActionBar(toolbar);}
 
 
     }
 
 
-}
+
